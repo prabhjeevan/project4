aaf763c5f653e6ace66bb85ba88c5b89e4783a2a
Prabhjeevan Kalra <prabhjeevan@MacBook-Pro.local>
2020-12-09 04:19:21 -0500

    first commit with user schema

---
+++ server/app.js
@@ -0,0 +1,27 @@
+const express = require ('express')
+const app = express()
+const mongoose = require('mongoose')
+const PORT = 3001
+const {MONGOURI} = require('./keys')
+
+
+require('./models/user')
+
+app.use(express.json())
+app.use(require('./routes/auth'))
+
+
+mongoose.connect(MONGOURI, {
+    useNewUrlParser:true,
+    useUnifiedTopology: true
+})
+mongoose.connection.on('connected', () =>{
+    console.log('connected to mongo')
+})
+mongoose.connection.on('error',(err) =>{
+    console.log('Error Connecting', err)
+})
+
+app.listen(PORT, () => {
+    console.log("server is running on", PORT) 
+})
---
+++ server/keys.js
@@ -0,0 +1,3 @@
+module.exports={
+     MONGOURI:"mongodb+srv://prabhjeevan:3b7hnd0CdOcP8B92@cluster0.qrxuz.mongodb.net/<dbname>?retryWrites=true&w=majority"
+}
